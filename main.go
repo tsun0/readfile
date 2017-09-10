c40a367223f0134a7b1550c5880d4f94a95949e5
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2017-09-10 17:52:15 +0900

    不要なコメントを削除、特定のファイルを読み込めるよう修正

--- main.go
+++ main.go
@@ -7,38 +7,15 @@ import (
 	"strings"
 )
 
-/*
-const BUFSIZE = 1024 // 読み込みバッファのサイズ
-
-func main() {
-	file, err := os.Open(`read.txt`)
-	if err != nil {
-		// Openエラー処理
-	}
-	defer file.Close()
-
-	buf := make([]byte, BUFSIZE)
-	for {
-		n, err := file.Read(buf)
-		if n == 0 {
-			break
-		}
-		if err != nil {
-			// Readエラー処理
-			break
-		}
-
-		fmt.Print(string(buf[:n]))
-	}
-}
-*/
-
 func main() {
 	const defaultLine = 2
+	const defaultFile = "read.txt"
 	var line int
-	flag.IntVar(&line, "line", defaultLine, "line to use")
+	var file string
+	flag.IntVar(&line, "line", defaultLine, "reading line")
+	flag.StringVar(&file, "file", defaultFile, "reading file")
 	flag.Parse()
-	ans, err := readLines("read.txt")
+	ans, err := readLines(file)
 	if err != nil {
 		panic(err)
 	}
